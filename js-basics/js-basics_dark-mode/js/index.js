b84c62745a3d1119dd5bef1d9c250e2e8c118736
Miok Park <trydd4408@gmail.com>
2023-11-06 14:07:29 +0100

    add js-basics_dark-mode challenge

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,24 @@
 console.clear();
 
+
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkModeBtn = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeBtn = document.querySelector('[data-js="light-mode-button"]');
+const toggleBtn = document.querySelector('[data-js="toggle-button"]');
+  
+   
+    // 버튼에 이벤트 리스너 추가
+    
+    darkModeBtn.addEventListener("click", function() {
+        bodyElement.classList.add("dark");
+    })
+    
+    lightModeBtn.addEventListener("click", () => {
+        bodyElement.classList.remove("dark");
+    })
+    
+    toggleBtn.addEventListener("click", () => {
+        bodyElement.classList.toggle("dark");
+    })
+  
+  
