8e5d1a7981f0aaae740e3af54ab342f986020b1f
Miok Park <trydd4408@gmail.com>
2023-11-10 15:38:00 +0100

    add handle-form-submit and checkbox-input challenges

--- js-forms/js-forms_calculator/js/index.js
+++ js-forms/js-forms_calculator/js/index.js
@@ -21,11 +21,29 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  // const formData = new FormData(event.target);
+  // const data = Object.fromEntries(formData);
+  // console.log("data: ", data);
+
+const formData = event.target.elements;
+console.log("formElements",formElements);
+// const numberA = event.target.elements.numberA.value;
+// const numberB = event.target.elements.numberB.value; 
+// or
+const numberA = Number(event.target.elements.numberA.value);
+const numberB = Number(event.target.elements.numberB.value);
+// console.log("numberA, numberB", typeof numberA, numberB);
+const operator = formElements.operator.value;
 
   let result;
 
   // --v-- write your code here --v--
+switch(operator) {
+  case "addition": //value name
+  result = add();
+break;
 
+}
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
