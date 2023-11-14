21864556ed81c022b2d70ea2a73c5a29fb53c6f8
Miok Park <trydd4408@gmail.com>
2023-11-14 11:14:07 +0100

    overwritten js-createelement_tost-messages

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -3,11 +3,17 @@ console.clear();
 const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
+const newToastMessage = document.createElement("li");
+newToastMessage.textContent = "Here new text"
+newToastMessage.classList.add("toast-container__message")
+
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  toastContainer.append(newToastMessage);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  // Exercise: Clear the stack of toast 
+  toastContainer.innerHTML = ""
 });
