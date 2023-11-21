1f1a6747c290b06f99d643ae991bd49383c5b4e9
Miok Park <trydd4408@gmail.com>
2023-11-21 11:52:21 +0100

    add async at star-wars

--- js-fetch/js-fetch_star-wars/js/index.js
+++ js-fetch/js-fetch_star-wars/js/index.js
@@ -41,6 +41,22 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+try{
+  const response = await fetch("https://swapi.dev/api/people");
+
+  if (response.ok){
+    const data = await response.jason();
+    console.log (data);
+    return data;
+  } else {
+    // Failure (Bad Response)
+    console.error("Bad Response");
+  }
+} catch (error) {
+  // Failure (Network error, etc)
+  console.error("An Error occurred");
+}
+  // fetch(); // ?
+
 }
