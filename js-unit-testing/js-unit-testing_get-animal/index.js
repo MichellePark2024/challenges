2d65262ae10850d227f321a6f4d02cc4d3bb6384
Miok Park <trydd4408@gmail.com>
2023-11-22 11:08:22 +0100

    add unit-testing_get-animal challenge

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,29 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+ if (animal=== "cats"){
+    return "I totally love cats!";
+ }
+ else if (typeof animal == "string") {
+    return `I like ${animal}!`
+}
+else if (typeof animal === "undefined") {
+    return "I do not like animals at all!"
+}
+}
+
+
+
+// export function getAnimal(animalPluraName){
+//     if (animalPluraName === "dog") {
+//         return "I like dogs!"
+//     }
+// }
+
+
+// export function getAnimal(animal) {
+//     if (animal === "cats") {
+//       return "I totally love cats!";
+//     } else {
+//       return "I don't know this animal.";
+//     }
+//   }
+  
