ff4f0a017a30d7b401cdfe4ba7732f913106de92
Miok Park <trydd4408@gmail.com>
2023-11-27 11:24:21 +0100

    add react-basics_hello-world challenge

--- react-basics/react-basics_hello-world/src/App.js
+++ react-basics/react-basics_hello-world/src/App.js
@@ -1,6 +1,18 @@
+// import React from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   return <div>Say hello...</div>;
+// }
+
 import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return <Child />
+}
+
+
+function Child (){
+  return <h1>Hello Michelle!</h1>;
 }
